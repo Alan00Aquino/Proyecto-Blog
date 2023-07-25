a1068895df339f109abf763a1e7b7948268e820d
carloslionelxd <carlos.lionel.barboza@gmail.com>
2023-07-25 00:24:20 -0300

    Agregando template de visualizar el detalle y boton de ver detalle

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -11,7 +11,9 @@ from django.urls import path
 from .views import PostListView, PostDetailView
 from . import views
 
+app_name = 'apps.posts'
+
 urlpatterns = [
     path('posts/', PostListView.as_view(), name = 'posts'),
-    path('posts/<int:id>/', PostDetailView.as_view(), name = 'post_individual.html'),
+    path('posts/<int:id>/', PostDetailView.as_view(), name = 'post_individual'),
 ]
