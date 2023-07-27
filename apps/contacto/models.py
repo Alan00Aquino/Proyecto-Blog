aa4b4161c6e728069d39eacbe2c66bc6f6114e90
Alan00Aquino <133703934+Alan00Aquino@users.noreply.github.com>
2023-07-27 20:13:23 -0300

    retoques

--- apps/contacto/models.py
+++ apps/contacto/models.py
@@ -6,9 +6,10 @@ from django.db import models
 class Contacto(models.Model):
     nombre = models.CharField(max_length = 100)
     email = models.EmailField()
+    asunto = models.CharField(max_length = 50, default = "sin asunto")
     mensaje = models.CharField(max_length = 255)
     numero_tel = models.CharField(max_length = 20)
 
-    def str(self):
-        return self.nombre
+    def __str__(self):
+        return self.asunto
 
--- apps/contacto/views.py
+++ apps/contacto/views.py
@@ -7,9 +7,10 @@ def contactoView (request):
     if request.method == 'POST':
         nombre = request.POST.get('nombre')
         email = request.POST.get('email')
+        asunto = request.POST.get('asunto')
         mensaje = request.POST.get('mensaje')
         numero_tel = request.POST.get('numero_tel')
-        contacto = Contacto(nombre=nombre, email=email, mensaje=mensaje, numero_tel=numero_tel)
+        contacto = Contacto(nombre=nombre, email=email,asunto=asunto, mensaje=mensaje, numero_tel=numero_tel)
         contacto.save()
 
     return render(request, 'contacto.html', {})
